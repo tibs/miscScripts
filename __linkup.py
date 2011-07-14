3fc47e812ee9269b0c8c1c460b9a624ac4cda676
Tony Ibbs <tibs@kynesim.co.uk>
2011-07-14 15:24:50 +0100

    Add a readme, and finetune stuff

--- __linkup.py
+++ __linkup.py
@@ -6,6 +6,7 @@ Find all of the Python scripts in this directory (except this one), and make
 soft links to them in the directory above, but without the trailing '.py'
 """
 
+import sys
 import os
 
 def linkup():
@@ -15,18 +16,28 @@ def linkup():
 
     files = os.listdir(this_dir)
     for name in files:
-        if name == this_file:
-            print 'Ignoring this file,', name
-        elif name.endswith('.py'):
-            link = name[:-3]
-            print 'Linking %s as ../%s'%(name, link)
-            try:
-                os.symlink(os.path.join(this_dir, name),
-                           os.path.join(parent_dir, link))
-            except OSError as e:
-                print '..', e
+        if name in (this_file, "readme.txt"):
+            print 'Ignoring', name
+            continue
+
+        if name[-1] in ('~', '#'):
+            print 'Ignoring', name
+            continue
+
+        base, ext = os.path.splitext(name)
+        print 'Linking %s as ../%s'%(name, base)
+        try:
+            os.symlink(os.path.join(this_dir, name),
+                       os.path.join(parent_dir, base))
+        except OSError as e:
+            print '..', e
 
 if __name__ == '__main__':
-    linkup()
+    args = sys.argv[1:]
+
+    if len(args) == 0:
+        linkup()
+    else:
+        print __doc__
 
 # vim: set tabstop=8 softtabstop=4 shiftwidth=4 expandtab:
