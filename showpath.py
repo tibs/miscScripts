3449a801647d5f200139dedb58977560933c14a8
Tony Ibbs <tony.ibbs@arm.com>
2017-10-12 10:30:40 +0100

    Do a minimal conversion to Python3 for showpath

--- showpath.py
+++ showpath.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """Print out the path (of various sorts)
 
@@ -10,8 +10,8 @@ Usage: %s [ <see below> ]
         -man            means print out MANPATH
         -python         means print out the Python path
                         (which is related to PYTHONPATH, but likely
-			to be longer to take account of Python's automatic
-			decision of where it's files live)
+                        to be longer to take account of Python's automatic
+                        decision of where it's files live)
         -path <what>    means print out the path <what>
                         (assumed to be a colon separated path list on Unix,
                          and a semicolon separated path list on NT)
@@ -19,7 +19,7 @@ Usage: %s [ <see below> ]
 
         -help           prints out this text
         -version        prints out the version information
-	-history        prints out the history information
+        -history        prints out the history information
 
 The environment variable used as the path name will be tried in uppercase,
 lowercase and capitalised if it is not found as requested.
@@ -32,6 +32,7 @@ __version__ = "1.1 (Tibs)"
 __history__ = """\
 1998-04-21: Created by Tibs
 1999-11-05: Now works on Unix and NT
+2017-10-12: Assume Python 3 (and do minimal fixes for that)
 """
 
 import sys
@@ -52,7 +53,7 @@ def environ(item):
     Returns the actual environment variable it used, and its value
     """
 
-    if os.environ.has_key(item):
+    if item in os.environ:
         return (item,os.environ[item])
     else:
         other = []
@@ -64,50 +65,46 @@ def environ(item):
         if cap != item: other.append(cap)
 
         for name in other:
-            if os.environ.has_key(name):
+            if name in os.environ:
                 return (name,os.environ[name])
 
-        raise KeyError,\
-              "There is no environment variable %s (also tried %s)"%\
-              (item,string.join(other,", "))
+        raise KeyError("There is no environment variable %s (also tried %s)" % (item,string.join(other,", ")))
 
 
 
 # ------------------------------------------------------------
 def print_usage(argv0):
     script_name = string.split(argv0, os.sep)[-1]
-    print __doc__%(script_name)
+    print(__doc__%(script_name))
 
 
 def print_envpath(envar):
     try:
         (name,path) = environ(envar)
-    except KeyError,what:
-        print what
+    except KeyError as what:
+        print('KeyError', what)
         return
 
-    print_path(name,string.split(path,PATH_DELIM))
+    print_path(name, path.split(PATH_DELIM))
 
 
 def print_path(name,path):
-    print "Path %s:"%name
+    print("Path %s:" % name)
     count = {}
     index = 0
     for item in path:
         index = index + 1
-        print "  %2d: %s"%(index,item)
-        if count.has_key(item):
+        print("  %2d: %s" % (index,item))
+        if item in count:
             count[item] = count[item] + 1
         else:
             count[item] = 1
-    keys = count.keys()
-    keys.sort()
-    for key in keys:
+    for key in sorted(count):
         if count[key] > 1:
             if count[key] == 2:
-                print "Item %s occurs twice"%(key)
+                print("Item %s occurs twice" % (key))
             else:
-                print "Item %s occurs %d times"%(key,count[key])
+                print("Item %s occurs %d times" % (key,count[key]))
 
 
 # ------------------------------------------------------------
@@ -129,10 +126,10 @@ def main():
     elif arg_list[0] == "-help":
         print_usage(sys.argv[0])
     elif arg_list[0] == "-version":
-        print "Version:",__version__
+        print("Version:",__version__)
     elif arg_list[0] == "-history":
-	print "History:"
-        print __history__
+        print("History:")
+        print(__history__)
     elif arg_list[0] == "-ld":
         print_envpath("LD_LIBRARY_PATH")
     elif arg_list[0] == "-man":
@@ -152,9 +149,9 @@ if __name__ == "__main__":
 
 
 # ----------------------------------------------------------------------
-# vim: set filetype=python expandtab shiftwidth=4:
 # [X]Emacs local variables declaration - place us into python mode
 # Local Variables:
 # mode:python
 # py-indent-offset:4
 # End:
+# vim: set tabstop=8 softtabstop=4 shiftwidth=4 expandtab:
